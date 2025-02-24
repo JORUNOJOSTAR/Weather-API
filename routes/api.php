28f6e962d27e541b5bcd7e4546c8496a45ad5127
JOJO <nwin919191@gmail.com>
2025-02-24 17:00:13 +0900

    Add throttle for api

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,10 @@ use App\Http\Controllers\v1\WeatherController;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix'=>'v1','namespace','App\Http\Controllers\v1\WeatherController'],function(){
+Route::group(['prefix'=>'v1','namespace'=>'App\Http\Controllers\v1\WeatherController','middleware'=>['throttle:weather']],function(){
     Route::get('/weather',WeatherController::class);
+});
+
+Route::get('/flush', function () {
+    Cache::flush();
 });
